cd26da6c7945474827fa5fbf3ba514f66c4e7813
AdrianBateman <adrian@a-bateman.co.uk>
2022-03-17 22:24:51 +0000

    init app

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,40 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: path.resolve(__dirname, './dist'),
+  configureWebpack: {
+    entry: './src/main.js',
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+      ]
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+          filename: "[name].css",
+          chunkFilename: "[name].css",
+          }),
+      new CleanWebpackPlugin(),
+    ],
+    watchOptions: {
+      poll: 1000,
+    },
+  }
 });
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[name].css",
+        }),
+    new CleanWebpackPlugin(),
+  ],
+  watchOptions: {
+    poll: 1000,
+  },
+}
